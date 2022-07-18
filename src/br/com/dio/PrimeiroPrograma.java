7a464510c82909dd7bcfc1c3e6bd2338a7f09180
alda510 <gustavoalda@gmail.com>
2022-07-18 15:33:45 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,10 +6,43 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         gato Gato = new gato();
         System.out.println(Gato);
-
         /*int a = 565;
         int b = 3;
 
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome=" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
